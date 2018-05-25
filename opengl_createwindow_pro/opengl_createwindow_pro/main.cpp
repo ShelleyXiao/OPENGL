9a8f1d70bba97770254634b370a6c2b98e4ca214
ShelleyXiao <xiao244164200@qq.com>
2018-05-25 16:47:06 +0800

    更改部分配置

--- opengl_createwindow_pro/opengl_createwindow_pro/main.cpp
+++ opengl_createwindow_pro/opengl_createwindow_pro/main.cpp
@@ -1,78 +1,273 @@
 #pragma comment(lib,"OpenGL32.lib")  
 
+#define GLEW_STATIC
+#include <GL\glew.h>
+#include <GLFW/glfw3.h>
+#include "Shader.h"
+#include "Camera.h"
+#include "Texture.h"
+#include <SOIL.h>
+
 #include <iostream>
 
-// GLEW
-#define GLEW_STATIC
-#include <GL/glew.h>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
 
-// GLFW
-#include <GLFW/glfw3.h>
 
-// Window dimensions
-const GLuint WIDTH = 800, HEIGHT = 600;
+// 键盘回调函数原型声明
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+// 鼠标移动回调函数原型声明
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos);
+// 鼠标滚轮回调函数原型声明
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 
-void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode) 
+// 场景中移动
+void do_movement();
+
+// 定义程序常量
+const int WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
+// 用于相机交互参数
+GLfloat lastX = WINDOW_WIDTH / 2.0f, lastY = WINDOW_HEIGHT / 2.0f;
+bool firstMouseMove = true;
+bool keyPressedStatus[1024]; // 按键情况记录
+GLfloat deltaTime = 0.0f; // 当前帧和上一帧的时间差
+GLfloat lastFrame = 0.0f; // 上一帧时间
+Camera camera(glm::vec3(0.0f, 0.0f, 4.0f));
+//glm::vec3 lampPos(0.8f, 0.8f, 0.5f);
+glm::vec3 lampPos(1.2f, 1.0f, 2.0f);
+
+
+GLfloat mixValue = 0.4f; // 纹理混合参数
+
+int main(int argc, char** argv)
 {
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+
+	if (!glfwInit())	// 初始化glfw库
 	{
-		glfwSetWindowShouldClose(window, GL_TRUE);
+		std::cout << "Error::GLFW could not initialize GLFW!" << std::endl;
+		return -1;
 	}
-}
 
-// The MAIN function, from here we start the application and run the game loop
-/**/
-int createwindow()
-{
-	std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
-	// Init GLFW
-	glfwInit();
-	// Set all the required options for GLFW
+	// 开启OpenGL 3.3 core profile
+	std::cout << "Start OpenGL core profile version 3.3" << std::endl;
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-	// Create a GLFWwindow object that we can use for GLFW's functions
-	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", nullptr, nullptr);
-	if (window == nullptr)
+	// 创建窗口
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT,
+		"Demo of Basic lighting", NULL, NULL);
+	if (!window)
 	{
-		std::cout << "Failed to create GLFW window" << std::endl;
+		std::cout << "Error::GLFW could not create winddow!" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
+	// 创建的窗口的context指定为当前context
 	glfwMakeContextCurrent(window);
-	// Set this to true so GLEW knows to use a modern approach to retrieving function pointers and extensions
-	glewExperimental = GL_TRUE;
-	// Initialize GLEW to setup the OpenGL Function pointers
-	if (glewInit() != GLEW_OK)
+
+	// 注册窗口键盘事件回调函数
+	glfwSetKeyCallback(window, key_callback);
+	// 注册鼠标事件回调函数
+	glfwSetCursorPosCallback(window, mouse_move_callback);
+	// 注册鼠标滚轮事件回调函数
+	glfwSetScrollCallback(window, mouse_scroll_callback);
+	// 鼠标捕获 停留在程序内
+//	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// 初始化GLEW 获取OpenGL函数
+	glewExperimental = GL_TRUE; // 让glew获取所有拓展函数
+	GLenum status = glewInit();
+	if (status != GLEW_OK)
 	{
-		std::cout << "Failed to initialize GLEW" << std::endl;
+		std::cout << "Error::GLEW glew version:" << glewGetString(GLEW_VERSION)
+			<< " error string:" << glewGetErrorString(status) << std::endl;
+		glfwTerminate();
 		return -1;
 	}
 
-	// Define the viewport dimensions
-	int width, height;
-	glfwGetFramebufferSize(window, &width, &height);
-	glViewport(0, 0, width, height);
+	// 设置视口参数
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	glfwSetKeyCallback(window, key_callback);
+	// Section1 准备顶点数据
+	// 指定顶点属性数据 顶点位置 颜色 纹理
+	GLfloat vertices[] = {
+		-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,0.0f, 0.0f,  // 0
+		0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,1.0f, 0.0f,  // 1
+		0.5f,  0.5f,  0.0f, 0.0f, 0.0f, 1.0f,1.0f, 1.0f,  // 2
+		-0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f,0.0f, 1.0f   // 3
+	};
 
-	// Game loop
+	GLushort indices[] = {
+		0, 1, 2,  // 第一个三角形
+		0, 2, 3   // 第二个三角形
+	};
+
+	//创建缓存对象
+	GLuint VAOId, VBOId, EBOId;
+
+	//创建绑定VAO对象
+	glGenVertexArrays(1, &VAOId);
+	glBindVertexArray(VAOId);
+
+	//创建绑定VBO对象 传递数据
+	glGenBuffers(1, &VBOId);
+	glBindBuffer(GL_ARRAY_BUFFER, VBOId);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+
+	//创建绑定ebo对象 传递数据
+	glGenBuffers(1, &EBOId);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBOId);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	// 指定解析方式，并启用顶点属性
+	//顶点位置属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,
+		8 * sizeof(GL_FLOAT), (GLvoid*)0);
+	glEnableVertexAttribArray(0);
+	// 顶点颜色属性
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE,
+		8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	// 顶点纹理坐标
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE,
+		8 * sizeof(GL_FLOAT), (GLvoid*)(6 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(2);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	Shader shader("triangle.vertex", "triangle.frag");
+
+	//准备纹理对象
+	GLuint textureId1 = TextureHelper::load2DTexture("../../../../resources/textures/wood.png");;
+	GLuint textureId2 = TextureHelper::load2DTexture("../../../../resources/textures/cat.png");;
+	
+
 	while (!glfwWindowShouldClose(window))
 	{
-		// Check if any events have been activiated (key pressed, mouse moved etc.) and call corresponding response functions
-		glfwPollEvents();
+		glfwPollEvents(); // 处理例如鼠标 键盘等事件
 
-		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		// 清除颜色缓冲区 重置为指定颜色
+		glClearColor(0.18f, 0.04f, 0.14f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		// Swap the screen buffers
-		glfwSwapBuffers(window);
+		//这里填写控制代码
+		glBindVertexArray(VAOId);
+		shader.Use();
+		//启用纹理单元 绑定纹理对象
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureId1);
+		glUniform1i(glGetUniformLocation(shader.Program, "tex1"), 0);
+
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureId2);
+		glUniform1i(glGetUniformLocation(shader.Program, "tex2"), 0);
+
+		glUniform1f(glGetUniformLocation(shader.Program, "mixValue"), mixValue);
+
+		// 使用索引绘制
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
+
+		glBindVertexArray(0);
+		glUseProgram(0);
+
+		glfwSwapBuffers(window); // 交换缓存
 	}
 
-	// Terminate GLFW, clearing any resources allocated by GLFW.
+	glDeleteVertexArrays(1, &VAOId);
+	glDeleteBuffers(1, &VBOId);
 	glfwTerminate();
 	return 0;
 }
 
+
+
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+			keyPressedStatus[key] = true;
+		else if (action == GLFW_RELEASE)
+			keyPressedStatus[key] = false;
+	}
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE); // 关闭窗口
+	}
+
+	else if (key == GLFW_KEY_S && action == GLFW_PRESS)
+	{
+		mixValue += 0.05f;
+		if (mixValue > 1.0f)
+			mixValue = 1.0f;
+	}
+	else if (key == GLFW_KEY_A && action == GLFW_PRESS)
+	{
+		mixValue -= 0.05f;
+		if (mixValue < 0.0f)
+			mixValue = 0.0f;
+	}
+}
+
+
+
+void mouse_move_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouseMove) // 首次鼠标移动
+	{
+		lastX = xpos;
+		lastY = ypos;
+		firstMouseMove = false;
+	}
+
+	GLfloat xoffset = xpos - lastX;
+	GLfloat yoffset = lastY - ypos;
+
+	lastX = xpos;
+	lastY = ypos;
+
+	camera.handleMouseMove(xoffset, yoffset);
+}
+
+
+// 由相机辅助类处理鼠标滚轮控制
+void mouse_scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.handleMouseScroll(yoffset);
+}
+
+// 由相机辅助类处理键盘控制
+void do_movement()
+{
+
+	if (keyPressedStatus[GLFW_KEY_W])
+		camera.handleKeyPress(FORWARD, deltaTime);
+	if (keyPressedStatus[GLFW_KEY_S]) {
+		if (mixValue == 1.0f)
+		{
+			mixValue = 0.1f;
+		}
+		else {
+			mixValue += 0.05f;
+		}
+	}
+		
+	if (keyPressedStatus[GLFW_KEY_A]) {
+		if (mixValue == 0.1f)
+		{
+			mixValue = 1.0f;
+		}
+		else {
+			mixValue -= 0.05f;
+		}
+	}
+		
+	if (keyPressedStatus[GLFW_KEY_D])
+		camera.handleKeyPress(RIGHT, deltaTime);
+}
